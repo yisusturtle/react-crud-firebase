dcb778417cc9c9690caccfb9d44da00c2643dd13
Jesús Narbona <60108037+yisusturtle@users.noreply.github.com>
2022-02-10 13:38:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0K1j5fkfTdxn3gOHop3nZw8I3Vta60uk",
-  authDomain: "crud-react-a3f31.firebaseapp.com",
-  projectId: "crud-react-a3f31",
-  storageBucket: "crud-react-a3f31.appspot.com",
-  messagingSenderId: "99094195327",
-  appId: "1:99094195327:web:11e4eb45e5356e3bdca34a"
+  //Some personal info
 };
 
 // Initialize Firebase
